fad9d3dc3136573ce16085d92afce1faabc1f15f
DESKTOP-MSJPKQI\dawso <dawsonlydiag@gmail.com>
2026-01-04 15:03:28 -0600

    april tags

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Manual.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Manual.java
@@ -8,12 +8,12 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 public class Manual extends OpMode {
     public Mecanum mecanum = new Mecanum();
     public Yeeter yeeter = new Yeeter();
-    //public AprilTag_9968 aTag = new AprilTag_9968();
-    //   double kp = 0.1;
-    //   double desiredBearingAngle = 0.0;
-    //   double bearingAngle = 0.0;
-    //   double bearingMotorPower = 0.0;
-    //   double targetingDeadband = 0.05;
+    public AprilTag_9968 aTag = new AprilTag_9968();
+    double kp = 0.1;
+    double desiredBearingAngle = 0.0;
+    double bearingAngle = 0.0;
+    double bearingMotorPower = 0.0;
+    double targetingDeadband = 0.05;
 
     //100% at 12 ft
     //90% at 10 ft
@@ -24,7 +24,7 @@ public class Manual extends OpMode {
 
     public void init() {
         mecanum.init(hardwareMap);
-        //aTag.init(hardwareMap);
+        aTag.init(hardwareMap);
         yeeter.init(hardwareMap);
     }
 
@@ -41,17 +41,17 @@ public class Manual extends OpMode {
             yeeter.resetPark();
         }
         // position 2
-        else if (gamepad2.circle){
+        else if (gamepad2.circle) {
             yeeter.yeetAllElements(0.70, 0.70, 290);
             yeeter.resetPark();
         }
         //position 3
-        else if (gamepad2.triangle){
+        else if (gamepad2.triangle) {
             yeeter.yeetAllElements(0.75, 0.78, 270);
             yeeter.resetPark();
         }
         // long shot
-        else if (gamepad2.square){
+        else if (gamepad2.square) {
             yeeter.yeetAllElements(0.90, 0.90, 265);
             yeeter.resetPark();
         }
@@ -59,19 +59,19 @@ public class Manual extends OpMode {
             yeeter.launchOne(0.60, 330);
             yeeter.resetPark();
         }
-        else if (gamepad2.dpad_right){
+        else if (gamepad2.dpad_right) {
             yeeter.launchOne(0.70, 290);
             yeeter.resetPark();
         }
-        else if (gamepad2.dpad_up){
+        else if (gamepad2.dpad_up) {
             yeeter.launchOne(0.80, 270);
             yeeter.resetPark();
         }
-        else if(gamepad2.dpad_left){
+        else if (gamepad2.dpad_left) {
             yeeter.launchOne(0.90, 265);
             yeeter.resetPark();
         }
-        else if(gamepad2.left_bumper){
+        else if (gamepad2.left_bumper) {
             yeeter.intakeOn();
             yeeter.resetPark();
         }
@@ -79,47 +79,42 @@ public class Manual extends OpMode {
             yeeter.resetLaunchSequence();
         }
 
-//        if(gamepad2.left_bumper){
-//            yeeter.intake();
-//        }
+        // Save CPU resources;
+        // can resume streaming when needed.
+        if (gamepad1.triangle) {
+            aTag.visionPortal.resumeStreaming();
 
-        // Save CPU resources; can resume streaming when needed.
-//              if (gamepad2.dpad_down) {
-//                 aTag.visionPortal.stopStreaming();
-//              }
-//              else if (gamepad2.dpad_up) {
-//                 aTag.visionPortal.resumeStreaming();
-//              }
-//
-//              aTag.runAprilTag(telemetry, gamepad2);
-//              bearingAngle = aTag.getRobotBearing();
-//
-//         Y = kp * (desired_angle - actual_angle)
-//              bearingMotorPower = kp * (desiredBearingAngle - bearingAngle);
-//
-//              if (gamepad2.square) {
-//                 // Limit range of targeting power between -1 and 1
-//                 if (bearingMotorPower > 1) {
-//                    bearingMotorPower = 1;
-//                 }
-//                 else if (bearingMotorPower < -1) {
-//                    bearingMotorPower = -1;
-//                 }
-//
-//         Add targeting power deadband
-//                 if (Math.abs(bearingMotorPower) < targetingDeadband) {
-//                    bearingMotorPower = 0.0;
-//                 }
-//
-//                 telemetry.addData("bearingPower: ", bearingMotorPower);
-//                 mecanum.setEachMecanumPower(-bearingMotorPower, bearingMotorPower, bearingMotorPower, -bearingMotorPower);
+            aTag.runAprilTag(telemetry, gamepad2);
+            bearingAngle = aTag.getRobotBearing();
 
+            // Y = kp * (desired_angle - actual_angle)
+            bearingMotorPower = kp * (desiredBearingAngle - bearingAngle);
+
+
+            // Limit range of targeting power between -1 and 1
+            if (bearingMotorPower > 1) {
+                bearingMotorPower = 1;
+            } else if (bearingMotorPower < -1) {
+                bearingMotorPower = -1;
+            }
+
+            // Add targeting power deadband
+            if (Math.abs(bearingMotorPower) < targetingDeadband) {
+                bearingMotorPower = 0.0;
+            }
+
+            //telemetry.addData("bearingPower: ", bearingMotorPower);
+            mecanum.setEachMecanumPower(-bearingMotorPower, bearingMotorPower, bearingMotorPower, -bearingMotorPower);
+
+        } else {
+            aTag.visionPortal.stopStreaming();
+        }
         telemetry.update();
     }
 
     @Override
     public void stop() {
         // Save more CPU resources when camera is no longer needed.
-        //      aTag.visionPortal.close();
+        aTag.visionPortal.close();
     }
 }
